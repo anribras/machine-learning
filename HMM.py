08a23e1db842faafc0361467399119238f5fe22b
anribras <bravo911@163.com>
2018-04-24 14:17:31 +0800

    updated

--- HMM.py
+++ HMM.py
@@ -1,17 +1,7 @@
 # -*- coding: utf-8 -*-
 # HMM.py
 
-
 import numpy as np
-
-#   | 0.5 0.3 0.2 |    |0.5 0.5|
-# A=| 0.3 0.5 0.2 | B= |0.4 0.6|
-#   | 0.2 0.3 0.5 |    |0.7 0.3|
-A=[[0.5,0.2,0.3],[0.3,0.5,0.2],[0.2,0.3,0.5]];
-B=[[0.5,0.5],[0.4,0.6],[0.7,0.3]];
-Pi=[0.2,0.4,0.4];
-obs = [0,1,0];
-
 
 # delta nx1
 # A nxn
@@ -32,10 +22,6 @@ def max_delta(delta,A):
 def veterbi_cal(Pi,A,B,obs):
     seq = obs;
     times = len(seq);
-    A =np.array(A);
-    B=np.array(B);
-    Pi =np.array(Pi);
-    obs =np.array(obs);
     #n为状态空间大小，m为观测空间大小
     [n,m]=B.shape;
     # delta初始化, 每1列代表1次观测,一共
@@ -71,7 +57,70 @@ def veterbi_cal(Pi,A,B,obs):
 
 
 
-if __name__ == '__main__':
 
-    print('veterbi cal:',veterbi_cal(Pi,A,B,obs));
+# 计算观测obs的概率
+# 有前向和后向两种实现方法,还可以综合使用，得出的结果都应该是一致的
+# 输入obs = [0 1 0] (红白红)
+def forward_method(Pi,A,B,obs):
+    times = len(obs);
+    #n为状态空间大小，m为观测空间大小
+    [n,m]=B.shape;
+    #alpha 初始化 alpha(i)全部保存下来
+    alpha = np.zeros([n,times]);
+    for i in range(times):
+        if (i == 0):
+            alpha[:,i] = Pi * B[:,obs[i]];
+        else:
+            alpha[:,i] = np.dot(A.T,alpha[:,i-1]) * B[:,obs[i]];
+    #final
+    p = sum(alpha[:,i]);
+    return p,alpha;
+
+def backward_method(Pi,A,B,obs):
+    times = len(obs);
+    #n为状态空间大小，m为观测空间大小
+    [n,m]=B.shape;
+    #后向倒序
+    beta = np.ones([n,times]);
+    for i in range(times-1,-1,-1):
+        if(i == times -1):
+            beta[:,i] = beta[:,i]; # keep 1
+        else:
+            beta[:,i] = np.dot(A,beta[:,i+1] * B[:,obs[i+1]]);
+    #final
+    p = sum(Pi * B[:,obs[i]] * beta[:,i]);
+
+    return p,beta;
+
+# 可以计算观测预测时刻t,某状态为qi的概率;
+# 可以计算观测预测时刻t,某状态为qi,时刻t+1,某状态为qj的联合概率;
+# 根据观测预测某个状态在T内出现的期望值..
+def p_cal(Pi,A,B,obs):
+
+    [p1,alpha] = forward_method(Pi,A,B,obs);
+    [p2,beta]  = backward_method(Pi,A,B,obs);
+
+    #666
+    p3 = sum (np.dot(A , B[:,obs[-1]] * beta[:,-1]) * alpha[:,-2]);
+
+    return p1,p2,p3
+
+if __name__ == '__main__':
+    #   | 0.5 0.3 0.2 |    |0.5 0.5|
+    # A=| 0.3 0.5 0.2 | B= |0.4 0.6|
+    #   | 0.2 0.3 0.5 |    |0.7 0.3|
+    A=[[0.5,0.2,0.3],[0.3,0.5,0.2],[0.2,0.3,0.5]];
+    B=[[0.5,0.5],[0.4,0.6],[0.7,0.3]];
+    Pi=[0.2,0.4,0.4];
+    obs = [0,1,0];
+
+    A =np.array(A);
+    B=np.array(B);
+    Pi =np.array(Pi);
+    obs =np.array(obs);
+
+    print('observe seqence probability cal(fp):',forward_method(Pi,A,B,obs));
+    print('observe seqence probability cal(bp):',backward_method(Pi,A,B,obs));
+    print('observe seqence probability cal(total):',p_cal(Pi,A,B,obs));
+    # print('veterbi cal:',veterbi_cal(Pi,A,B,obs));
 
